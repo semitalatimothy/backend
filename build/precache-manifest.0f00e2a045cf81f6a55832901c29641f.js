9adbad09ab6b08adea92792266dc04aa3d01a1a4
unknown <semitalatimothy@gmail.com>
2020-10-26 10:45:51 +0100

    Second commit

---
+++ build/precache-manifest.0f00e2a045cf81f6a55832901c29641f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d5fff64a62c2c3781b2a6af04770c26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1e8cddb6f9404fc380b",
+    "url": "/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "26ebe4944ff98d77e722",
+    "url": "/static/css/main.f5491c4b.chunk.css"
+  },
+  {
+    "revision": "f1e8cddb6f9404fc380b",
+    "url": "/static/js/2.1b2a0480.chunk.js"
+  },
+  {
+    "revision": "0a0844297ef0855e273bd429cd7da9ad",
+    "url": "/static/js/2.1b2a0480.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26ebe4944ff98d77e722",
+    "url": "/static/js/main.b43115bd.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
